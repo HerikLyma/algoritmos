2845caebe940506ba2e8700cc7a670b53c865797
HerikLyma <heriklyma@hotmail.com>
2017-11-07 20:43:32 -0200

    Novos métodos para a árvore binária de busca

--- ArvoreBinariaBusca/arvorebinariabusca.cpp
+++ ArvoreBinariaBusca/arvorebinariabusca.cpp
@@ -13,6 +13,34 @@ class Arvore
     };
     Elemento *raiz = nullptr;
 
+    Elemento *pai(Elemento *elemento, const TIPO &valor)
+    {
+        if(elemento)
+        {
+            if(valor > elemento->valor)
+            {
+                Elemento *direita = elemento->direita;
+                if(direita)
+                {
+                    if(direita->valor == valor)
+                        return elemento;
+                    return pai(direita, valor);
+                }
+            }
+            else
+            {
+                Elemento *esquerda = elemento->esquerda;
+                if(esquerda)
+                {
+                    if(esquerda->valor == valor)
+                        return elemento;
+                    return pai(esquerda, valor);
+                }
+            }
+        }
+        return nullptr;
+    }
+
     Elemento *inserir(Elemento *elemento, Elemento *novo)
     {
         if(!elemento)
@@ -58,6 +86,26 @@ class Arvore
         return elemento;
     }
 
+    Elemento *maximo(Elemento *elemento) const
+    {
+        if(elemento)
+        {
+            while(elemento->direita)
+               elemento = elemento->direita;
+        }
+        return elemento;
+    }
+
+    Elemento *minimo(Elemento *elemento) const
+    {
+        if(elemento)
+        {
+            while(elemento->esquerda)
+               elemento = elemento->esquerda;
+        }
+        return elemento;
+    }
+
     void deletar(Elemento *elemento)
     {
         if(elemento)
@@ -94,21 +142,82 @@ public:
     {
         imprimirDecrescente(raiz);
     }
+
+    TIPO minimo(bool *encontrou = nullptr) const
+    {
+        Elemento *aux = minimo(raiz);
+        if(encontrou)
+            *encontrou = aux != nullptr;
+        if(aux)
+            return aux->valor;
+        return TIPO();
+    }
+
+    TIPO maximo(bool *encontrou = nullptr) const
+    {
+        Elemento *aux = maximo(raiz);
+        if(encontrou)
+            *encontrou = aux != nullptr;
+        if(aux)
+            return aux->valor;
+        return TIPO();
+    }
+
+    TIPO pai(const TIPO &valor, bool *encontrou = nullptr)
+    {
+        Elemento *paiAux = pai(raiz, valor);
+        if(encontrou)
+            *encontrou = paiAux != nullptr;
+        if(paiAux)
+            return paiAux->valor;
+        return TIPO();
+    }
+
+    TIPO sucessor(const TIPO &valor)
+    {
+        Elemento *aux = buscar(raiz, valor);
+        if(aux)
+        {
+            if(aux->direita)
+                return minimo(aux->direita)->valor;
+            else
+            {
+                Elemento *paiAux = pai(raiz, valor);
+                while(paiAux && (aux = paiAux->direita))
+                {
+                    aux = paiAux;
+                    paiAux = pai(raiz, paiAux->valor);
+                }
+                return aux->valor;
+            }
+        }
+        return valor;
+    }
 };
 
 int main()
 {
     Arvore<int> arvore;
-    arvore.inserir(10);
-    arvore.inserir(9);
-    arvore.inserir(11);
     arvore.inserir(8);
-    arvore.inserir(12);
+    arvore.inserir(3);
+    arvore.inserir(10);
+    arvore.inserir(1);
+    arvore.inserir(6);
+    arvore.inserir(14);
+    arvore.inserir(4);
+    arvore.inserir(7);
+    arvore.inserir(13);
     arvore.imprimirCrescente();
     cout << "\n\n";
     arvore.imprimirDecrescente();
     cout << boolalpha;
-    cout << "\n\nExiste 9? " << arvore.existe(9);
-    cout << "\nExiste 19? " << arvore.existe(19) << "\n\n";
+    cout << "\n\nExiste 8? " << arvore.existe(8);
+    cout << "\nExiste 18? " << arvore.existe(18) << "\n\n";
+    cout << "Minimo: " << arvore.minimo() << '\n';
+    cout << "Maximo: " << arvore.maximo() << '\n';
+    cout << "Pai de 3: " << arvore.pai(3) << '\n';
+    cout << "Pai de 13: " << arvore.pai(13) << '\n';
+    cout << "Sucessor de 3: " << arvore.sucessor(3) << '\n';
+    cout << "Sucessor de 7: " << arvore.sucessor(7) << '\n';
     return 0;
 }
